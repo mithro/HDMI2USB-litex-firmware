bd1c67bd6621ee94c707dc793461fd7de175ad4a
Carl Karsten <carl@personnelware.com>
2018-04-08 01:58:38 -0500

    add parameters for all the boards and things.

--- jenkins/tests/ck_version.py
+++ jenkins/tests/ck_version.py
@@ -11,13 +11,8 @@ from pprint import pprint
 
 
 def git_rev(
-        host='code.timvideos.us',
-        base='HDMI2USB-firmware-prebuilt',
-        platform='opsis',
-        target='hdmi2usb',
-        cpu='lm32',
-        channel="unstable",
-        ):
+        host, base, platform, target, cpu,
+        channel):
 
     url = "https://{host}/{base}/{platform}/{target}/{cpu}/channels.txt".format(**locals()) ## save me f-strings
 
@@ -72,7 +67,22 @@ def board_rev(tty):
 def pars_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--tty" )
+    parser.add_argument("--tty", default="/dev/ttyACM0",
+            help='default: %(default)s')
+
+    parser.add_argument("--host", default="code.timvideos.us" ,
+            help='default: %(default)s')
+    parser.add_argument("--base", default="HDMI2USB-firmware-prebuilt",
+            help='default: %(default)s')
+    parser.add_argument("--platform", default="opsis",
+            help='default: %(default)s')
+    parser.add_argument("--target", default="hdmi2usb",
+            help='default: %(default)s')
+    parser.add_argument("--cpu", default="lm32",
+            help='default: %(default)s')
+
+    parser.add_argument("--channel", default="unstable",
+            help='default: %(default)s')
 
     parser.add_argument("-v --verbose", action="store_true" )
     parser.add_argument("--version", action="store_true" )
@@ -84,9 +94,14 @@ def pars_args():
 
 def test(args):
 
-    gr = git_rev()
+    gr = git_rev(
+        args.host, args.base, args.platform, args.target, args.cpu,
+        args.channel)
+
     br = board_rev(args.tty)
+
     assert gr==br
+
     print("{gr} == {br}".format( gr=gr, br=br))
     print("expected version test passed.")
 
